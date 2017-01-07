4615a393076ea8d8e252e2c5655c6ad6ee9e58ac
Shuka Takakuma <shuka.takakuma@gmail.com>
2017-01-08 00:30:00 +0900

    add head actor bean definition

---
+++ src/main/java/linebot/tpb/di/annotation/HeadActorRef.java
@@ -0,0 +1,13 @@
+package linebot.tpb.di.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Qualifier
+public @interface HeadActorRef {
+}
